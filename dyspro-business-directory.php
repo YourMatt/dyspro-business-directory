eb1949945244fa4668fc81c865a5b7a35e72b3f4
Matt Sadowski <github@yourmatt.com>
2014-09-18 09:01:40 -0600

    Create dyspro-business-directory.php

---
+++ dyspro-business-directory.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Dyspro Business Directory
+Plugin URI:
+Description: Creates a new business content type and allows for linking to user profiles and adding specific business list fields.
+Version: 0.9
+Author: Dyspro Media
+Author URI: http://dyspromedia.com
+*/
+
+?>
